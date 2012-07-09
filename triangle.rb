dcb185cf8c3e6f926c131cd7342878062e71aade
Joshua Rowley <joshua.rowley@nyu.edu>
2012-07-08 21:40:57 -0400

    160/280 Learn exceptions and part of iterations

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,7 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
   if a == 0 || b == 0 || c == 0
   	raise TriangleError, "one or more sides is equivalent to 0"
   elsif a < 0 || b < 0 || c < 0 then
